f2f438faed9b3b1a692c92b922fe702702666d6c
d.golubev <dmitrii.golubev@deliveryhero.com>
2021-06-09 16:46:36 +0200

    Update cleanup the code

--- cmd/p2p/main.go
+++ cmd/p2p/main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"time"
-)
-
 func main() {
-	mux := http.ServeMux{}
-	var items []map[string]interface{}
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			time.Sleep(10 * time.Millisecond)
-			b, _ := json.Marshal(items)
-			_, _ = w.Write(b)
-			items = []map[string]interface{}{}
-		} else {
-			time.Sleep(20 * time.Millisecond)
-			var d map[string]interface{}
-			_ = json.NewDecoder(r.Body).Decode(&d)
-			items = append(items, d)
-			_, _ = w.Write([]byte("persisted"))
-			fmt.Println("persisted", d)
-		}
-	})
-	_ = http.ListenAndServe(":8881", &mux)
+	// here should be your peer-to-peer implementation
 }
